55cd82e3867a8709bd8cf671daf0b65f8cf09975
peter.tackage <peter.tackage@gmail.com>
2013-10-07 21:19:23 +0200

    Remove .json suffix from API endpoints
    This is now set via the ScRequestInterceptor

--- src/main/java/com/moac/android/wallpaperdemo/api/SoundCloudApi.java
+++ src/main/java/com/moac/android/wallpaperdemo/api/SoundCloudApi.java
@@ -8,15 +8,15 @@ import retrofit.http.Query;
 import java.util.List;
 
 public interface SoundCloudApi {
-    @GET("/tracks.json")
+    @GET("/tracks")
     public List<Track> getTrackList(@Query("genre") String _genre,
                                  @Query("tags") String _tags, @Query("offset") long _offset);
 
-    @GET("/tracks.json")
+    @GET("/tracks")
     public void getTrackList(@Query("genre") String _genre,
                                     @Query("tags") String _tags, @Query("offset") long _offset, Callback<List<Track>> _callback);
 
-    @GET("/tracks.json")
+    @GET("/tracks")
     public Track getTrack(@Query("genre") String _genre,
                                  @Query("tags") String _tags, @Query("offset") long _offset);
 }
