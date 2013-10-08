82d81c359d83383ded9a59bbf411cc7f47302470
peter.tackage <peter.tackage@gmail.com>
2013-10-08 14:27:01 +0200

    Add compile time debug switch to WaveformProcessor

--- src/main/java/com/moac/android/wallpaperdemo/WaveformProcessor.java
+++ src/main/java/com/moac/android/wallpaperdemo/WaveformProcessor.java
@@ -9,6 +9,7 @@ import java.util.Arrays;
 public class WaveformProcessor implements BitmapProcessor {
 
     private static final String TAG = WaveformProcessor.class.getSimpleName();
+    private static final boolean DEBUG = false;
 
     public float[] transform(Bitmap _bitmap) {
         // TODO Test performance for extractAlpha vs Color.alpha()
@@ -21,9 +22,11 @@ public class WaveformProcessor implements BitmapProcessor {
         for(int x = 0; x < width; x++) {
             for(int y = 0; y < centreLine; y++) {
                 if(Color.alpha(_bitmap.getPixel(x,y)) == 0) {
-             //       Log.v(TAG, "transform() - found transparent pixel: " + x +"/" +y);
+                    if(DEBUG)
+                        Log.v(TAG, "transform() - found transparent pixel: " + x +"/" +y);
                     normalizedAmplitude[x] = (centreLine - y) / centreLine;
-             //       Log.v(TAG, "normalised amp for x: " + x + " is: " + normalizedAmplitude[x]);
+                    if(DEBUG)
+                        Log.v(TAG, "normalised amp for x: " + x + " is: " + normalizedAmplitude[x]);
                     break; // next sample in x.
                 }
             }
